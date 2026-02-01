cc328df77d5dbdbc6a91f825b873f230b2b7f756
AnimatingLegend <thesfmgod@gmail.com>
2026-02-01 00:00:22 -0500

    [FIX] - XP Traffic jam

--- utils/events/messageCreate.js
+++ utils/events/messageCreate.js
@@ -8,16 +8,25 @@ module.exports = {
 
           if (!guildXPEnabled(msg.guild.id)) return;
           if (!channelXPEnabled(msg.channel.id)) return;
-          if (!getLvlUpChannel(msg.guild.id)) return;
 
           console.log(`Server XP Enabled = **${guildXPEnabled(msg.guild.id)}**`);
           console.log(`Channel XP Enabled = **${channelXPEnabled(msg.channel.id)}**`);
 
           const xpAmount = Math.floor(Math.random() * 10) + 5;
-
           const result = addXP(msg.author.id, msg.guild.id, xpAmount);
+
           if (result.leveledUp) {
-               msg.channel.send(`:tada:  **<@${msg.author.id}>** has reached **level ${result.level}**!`);
+               const lvlUp_channelID = getLvlUpChannel(msg.guild.id);
+
+               // --- Try and find the dedicated channel, otherwise, use the current channel --- \\
+               const target_channel = msg.guild.channels.cache.get(lvlUp_channelID);
+
+               try {
+                    await target_channel.send(`:tada: **<@${msg.author.id}>** has reached **level ${result.level}**!`);
+               } catch (err) {
+                    // --- Fallback to current channel if the dedicated channel cant be found --- \\
+                    msg.channel.send(`: tada:  **<@${msg.author.id}>** has reached **level ${result.level}**!`);
+               }
           }
      }
 };
