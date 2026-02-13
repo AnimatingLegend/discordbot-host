4dbee03300a0965a2d111a2c9c03f500941e03bf
AnimatingLegend <thesfmgod@gmail.com>
2026-02-13 02:15:31 -0500

    [REMOVAL] - `random-emojis` support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
      - Fixed issue where if you type **any** guild command, the entire bot client would crash, or display an error message.
 - **Logging Issues**:
      - Fixed issue where `guild_xp_settings` and `channel_xp_settings` would log `[TRUE]`, even when disabled.
+### Removed
+- `random-emojis` support.
 
 ## [2.4.0] - [2026-02-10]
 ### Complete Source Code Overhaul:
--- source/utils/random-colors.js
+++ source/utils/random-colors.js
@@ -1,5 +1,4 @@
-const COLORS = require('../database/data/colors.json');
-const HEX = Object.keys(COLORS);
+const COLORS_JSON = require('../database/data/colors.json');
 
 module.exports = {
      /**
@@ -7,16 +6,28 @@ module.exports = {
       * @param {string[]} HEX - Array of colors from `colors.json`
       */
      random: () => {
+          /**
+           * Get all HEX codes from `colors.json`
+           * @type {string[]}
+           */
+          const HEX_CODES = Object.keys(COLORS_JSON);
+
           // If JSON is empty or missing, return default color.
-          if (!HEX || HEX.length === 0) return '#5865F2';
+          if (!HEX_CODES || HEX_CODES.length === 0) return '#5865F2';
 
           /**
            * Go through a random index of colors, and select one.
            * @returns {string} - HEX color
            */
-          const RAN_INDEX = Math.floor(Math.random() * HEX.length);
+          const RAN_INDEX = Math.floor(Math.random() * HEX_CODES.length);
           const SEL_HEX = HEX[RAN_INDEX];
 
           return SEL_HEX.startsWith('#') ? SEL_HEX : `${SEL_HEX}`;
+     },
+
+     // === Get a specific color === \\
+     static: (name) => {
+          const FIND_COLOR = Object.keys(COLORS_JSON).find(key => key.toLowerCase() === name.toLowerCase());
+          return FIND_COLOR ? COLORS_JSON[FIND_COLOR] : '#5865F2';
      },
 };
